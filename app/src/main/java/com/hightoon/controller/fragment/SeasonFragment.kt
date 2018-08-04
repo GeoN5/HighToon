b98cb2c3fff636868233b94549fc30e558b976e8
GeoN5 <sgepyh2916@naver.com>
2018-08-05 01:24:58 +0900

    error solve

--- app/src/main/java/com/hightoon/controller/fragment/SeasonFragment.kt
+++ app/src/main/java/com/hightoon/controller/fragment/SeasonFragment.kt
@@ -2,14 +2,10 @@ package com.hightoon.controller.fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v4.view.ViewPager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.hightoon.R
-import com.hightoon.controller.adapter.TabPagerAdapter
-import android.support.design.widget.TabLayout
-import com.hightoon.controller.adapter.TabPagerAdapter
 
 
 
